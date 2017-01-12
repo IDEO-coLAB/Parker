c5cd1c6e3704e7685b349296bbc98dca8889b442
The-Chanman <ecphylo@gmail.com>
2017-01-12 09:41:24 -0500

    weather from Cambridge

--- sensor.js
+++ sensor.js
@@ -1,137 +1,38 @@
 const Nomad = require('nomad-stream')
-const moment = require('moment')
-const Particle = require('particle-api-js')
-
-const credentials = require('./particle-login')
-
-const particle = new Particle()
 const nomad = new Nomad()
+const fetch = require('node-fetch')
 
-// Particle Device Setup
-// Atomic node 1
-const deviceID = '2b0045000247353138383138'
+let instance = null  // the nomad instance
+const pollFrequency =  30 * 1000  // 60 seconds
+const url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Cambridge%2C%20Massachusetts%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
 
-let instance = null
-let lastPub = null
-let token
+console.log(url)
 
-const defaultPublishData = { 
-  parkertemperature: {
-    data: "",
-    time: "",
-    description: "The Status of the temperature in the room at IDEO CoLab"
-  },
-  parkerpressure: {
-    data: "",
-    time: "",
-    description: "The Status of the pressure in the room at IDEO CoLab"
-  },
-  parkergoaltemp: {
-    data: "",
-    time: "",
-    description: "The Status of the goal temperature in the room at IDEO CoLab"
-  },
-  parkeraltitude: {
-    data: "",
-    time: "",
-    description: "The Status of the altitude in the room at IDEO CoLab"
-  }
-}
-const timeBetween =  60 * 1000 // 2 minutes
-const timeThreshold = 4 * 60 * 60 * 1000 // 4 hours
-
-class DataMaintainer {
-  constructor(){
-    this.data = defaultPublishData
-  }
-  setValue(key, value){
-    let cleanedKey = this.cleanKey(key)
-    if(cleanedKey in this.data){
-      this.data[cleanedKey].data = value.data
-      this.data[cleanedKey].time = value.time
-    } else {
-      this.data[cleanedKey] = value
-    }
-  }
-  cleanKey(key){
-    let cleanedKey = key.replace(/\s+/, '\x01').split('\x01')[0]
-    cleanedKey = cleanedKey.toLowerCase()
-    return cleanedKey
-  }
-  getAll(){
-    return this.data
-  }
-  isAllFilled(){
-    return this.data["sensor"]["data"] && this.data["sensor"]["time"]
-  }
-  clear(){
-    this.data = defaultPublishData
-  }
-  toString(){
-    return JSON.stringify(this.data)
-  }
-}
-
-function getTime() {
-  return new moment()
-}
-
-//init data manager
-let dataManager = new DataMaintainer()
-
-particle.login(credentials)
-  .then(res => {
-    token = res.body.access_token
-    console.log(`Got Token: ${token}`)
-    return nomad.prepareToPublish()
-  })
-  .then((n) => {
-    instance = n
-    return instance.publishRoot('hello this data from the home of colab cambridge')
-  })
-  .then(() => {
-    //declaring last publish date
-    lastPub = getTime()
-    return particle.getEventStream({ deviceId: deviceID, auth: token })
-  })
-  .then(s => {
-    stream = s
-    stream.on('event', data => {
-      console.log(data)
-      try{dataManager.setValue(data.name, {data: data.data, time: data.published_at})}
-      catch(err){
-        console.log("DataMaintainer failed with error of " + err)
-      }
-      // this determines frequency of transmission 
-      let currentTime = getTime()
-      let timeSince = currentTime - lastPub
-      if (timeSince >= timeBetween){
-
-        console.log("timeSince >= timeBetween")
-
-        if (dataManager.isAllFilled){
-          // publish if everything is full
-          console.log("***************************************************************************************")
-          console.log(dataManager.getAll())
-          console.log("***************************************************************************************")
-
-          instance.publish(dataManager.toString())
-            .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
-          dataManager.clear()  
-          lastPub = currentTime
-        }
-      }
-      // if haven't receieved anything in the time frame
-      if (timeSince >= timeThreshold){
-        // publish what we got
-        instance.publish(dataManager.toString())
-          .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
-        console.log("***************************************************************************************")
-        console.log(dataManager.getAll())
-        console.log("***************************************************************************************")
-        dataManager.clear()  
-        lastPub = currentTime
-      }
+function getMessage() {
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => JSON.stringify(json))
+    .catch(err => {
+      console.log('getMessage error: ', err)
+      return err
     })
+}
+
+function startPoll(frequency) {
+  setInterval(() => {
+    getMessage()
+      .then((m) => {
+        console.log('fetched:', m)
+        return instance.publish(m)
+      })
+      .catch(console.log)
+  }, frequency)
+}
+
+nomad.prepareToPublish()
+  .then((node) => {
+    instance = node
+    return instance.publishRoot('hello')
   })
-  .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
+  .then(() => startPoll(pollFrequency))
+  .catch(console.log)
