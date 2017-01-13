c783cb404e889250274c85d16e4dde3939233fcb
iTwicKz <twickes32@ufl.edu>
2017-01-12 23:19:03 -0500

    Added content to suggestion cards

--- pages/home/index.js
+++ pages/home/index.js
@@ -40,12 +40,52 @@ class CarSpot extends Component {
             </div>
             <div className={s.textContainer}>
               <h1>Replace Insulation</h1>
-              <h2>When: <span className={s.urgent}>Sometime in the next year</span></h2>
-              <h2>How much: <span className={s.urgent}>$400-$1700</span></h2>
+              <h2>Your Energy Benefit: <span className={s.urgent}>Highly Recommended</span></h2>
+              <h2>Estimated Return: <span className={s.price}>$2000 annually</span></h2>
+              <h2>Cost: <span className={s.cost}>$400-$1700</span></h2>
               <p>We noticed your heater is working extra hard, especially compared to other homes similar to yours.
               We also noticed that it’s been really cold outside, so we think you might have heat escaping your home through the walls.
               You can prevent that by installing insulation in the walls. Imagine the stuffing inside of your thickest, warmest jacket….
               But in your walls! Mind. Blown.
+              </p>
+            </div>
+          </div>
+          <div className={s.suggestioncard}>
+            <div className={s.imageContainer}>
+            <img className={s.cardImage} src={"../../assets/images/insulation.png"} />
+            </div>
+            <div className={s.textContainer}>
+              <h1>Close Your Blinds</h1>
+              <h2>Your Energy Benefit: <span className={s.medium}>Recommended</span></h2>
+              <h2>Estimated Return: <span className={s.price}>$120 annually</span></h2>
+              <h2>Cost: <span className={s.cost}>$400-$1700</span></h2>
+              <p>We noticed that your top floor bedroom is typically warmer than anywhere else in the house. We noticed during the day it was very bright in that room, but the lights were off, which could mean your blinds/shades are open. It’s been sunny the past couple days, and that sunlight is probably heating up your room, so if you’d like to cooler, just try closing the blinds!
+              </p>
+            </div>
+          </div>
+          <div className={s.suggestioncard}>
+            <div className={s.imageContainer}>
+            <img className={s.cardImage} src={"../../assets/images/insulation.png"} />
+            </div>
+            <div className={s.textContainer}>
+              <h1>Replace Windows</h1>
+              <h2>Your Energy Benefit: <span className={s.low}>Currently Low</span></h2>
+              <h2>Estimated Return: <span className={s.price}>$400 annually</span></h2>
+              <h2>Cost: <span className={s.cost}>$500-$1500</span></h2>
+              <p>We noticed that your office has been colder than the rest of your home the past couple of days. It’s also been windy outside, and we think there might be a draft in your office. This could mean that your window is not closed. If it is closed, you may want to check that it is installed correctly and doesn’t have any cracks or gaps.
+              </p>
+            </div>
+          </div>
+          <div className={s.suggestioncard}>
+            <div className={s.imageContainer}>
+            <img className={s.cardImage} src={"../../assets/images/insulation.png"} />
+            </div>
+            <div className={s.textContainer}>
+              <h1>Be Cautious of Open Doors</h1>
+              <h2>Your Energy Benefit: <span className={s.urgent}>Highly Recommended</span></h2>
+              <h2>Estimated Return: <span className={s.price}>$40 annually</span></h2>
+              <h2>Cost: <span className={s.cost}>Free</span></h2>
+              <p>We noticed that your heater is working extra hard, especially compared to other homes similar to yours. We also noticed that it’s been really cold outside, and we’ve been detecting a draft in your living room, though your door is closed. You might want to check that your doggy-door is well-sealed.
               </p>
             </div>
           </div>
