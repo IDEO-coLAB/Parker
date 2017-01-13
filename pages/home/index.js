bb259bd833ee0a21163f936ae46c3790c089a32d
The-Chanman <ecphylo@gmail.com>
2017-01-13 01:45:43 -0500

    added the dynamic content that we are grabbing from the sensors and the web

--- pages/home/index.js
+++ pages/home/index.js
@@ -25,15 +25,15 @@ class CarSpot extends Component {
             <img className={s.floorimage} src={"../../assets/images/HouseSiteMockup_office.png"} />
             <div className={s.babyCard + ' ' + s.officeFloorCard + ' ' + s.interiorCard}>
               <h3>interior</h3>
-              <h1>60°</h1>
+              <h1>{(this.props.insideTemp)? this.props.insideTemp : "NA"}°</h1>
             </div>
             <div className={s.babyCard + ' ' + s.officeFloorCard + ' ' + s.exteriorCard}>
               <h3>exterior</h3>
-              <h1>43°</h1>
+              <h1>{(this.props.weatherTemp)? this.props.weatherTemp : "NA"}°</h1>
             </div>
             <div className={s.babyCard + ' ' + s.officeFloorCard + ' ' + s.setCard}>
               <h3>set temp</h3>
-              <h1>73°</h1>
+              <h1>{(this.props.goalTemp)? this.props.goalTemp : "NA"}°</h1>
             </div>
           </div>
           <div className={s.floor}>
@@ -50,11 +50,11 @@ class CarSpot extends Component {
           <div className={s.floor}>
             <img className={s.floorimage} src={"../../assets/images/HouseSiteMockup_living.png"} />
             <div className={s.babyCard + ' ' + s.livingFloorCard + ' ' + s.retainCard}>
-              <h3>closed</h3>
+              <h3>time retained</h3>
               <h1>60%</h1>
             </div>
             <div className={s.babyCard + ' ' + s.livingFloorCard + ' ' + s.heatCard}>
-              <h3>opened</h3>
+              <h3>time to heat</h3>
               <h1>40%</h1>
             </div>
           </div>
@@ -128,7 +128,7 @@ class HomePage extends Component {
   constructor(props) {
     super(props)
     this.state = {state:
-      { windSpeed: '', atmosphere: '', astronomy: '', conditions: '', elderHeartRate: '', elderLightSensor: '', elderGPS: '', elderAccelerometer: '', gasSensor: '', energyMain: '', electricityUsage: '', electricityAppliances: '', electricityEvents: '', water: ''}
+      { windSpeed: '', atmosphere: '', astronomy: '', conditions: '', insideTemp: '', insidePressure: '', goalTemp: '', insideAltitude: '', hvacStatus: '', hvacForce: ''}
     }
   }
 
@@ -143,7 +143,6 @@ class HomePage extends Component {
     let _this = this
     fetch('http://localhost:9000')
     .then(res => res.json())
-    .then(json => {
       // const state = [
       //   {available: true, price: 1},
       //   {available: false, price: 2},
@@ -152,20 +151,19 @@ class HomePage extends Component {
       const parsed = JSON.parse(json)
       debugger
       const state = {
-        windSpeed: parsed['QmWYaxscTPj79NHLnFEuUbKpZx3d4zHBK2ZRNgqrnF9s5n']['data']['channel']['wind']['speed'],
-        atmosphere: parsed['QmWYaxscTPj79NHLnFEuUbKpZx3d4zHBK2ZRNgqrnF9s5n']['data']['channel']['atmosphere'],
-        astronomy: parsed['QmWYaxscTPj79NHLnFEuUbKpZx3d4zHBK2ZRNgqrnF9s5n']['data']['channel']['astronomy'],
-        conditions: parsed['QmWYaxscTPj79NHLnFEuUbKpZx3d4zHBK2ZRNgqrnF9s5n']['data']['channel']['item']['condition'],
-        elderHeartRate: parsed['QmXUHQ2KBmSxaiEbB5eFVGGmSsPnd5KpnRqGWvtFUjWo5a']['data']['heartRate'],
-        elderLightSensor: parsed['QmXUHQ2KBmSxaiEbB5eFVGGmSsPnd5KpnRqGWvtFUjWo5a']['data']['lightSensor'],
-        elderGPS: parsed['QmXUHQ2KBmSxaiEbB5eFVGGmSsPnd5KpnRqGWvtFUjWo5a']['data']['gps'],
-        elderAccelerometer: parsed['QmXUHQ2KBmSxaiEbB5eFVGGmSsPnd5KpnRqGWvtFUjWo5a']['data']['accelerometer'],
-        gasSensor: parsed['QmdhnZLVkkPyEoyxGf238pPPMsKNjPS3cC3sFzSW3cbnQA']['gasSensor'],
-        energyMain: parsed['QmdhnZLVkkPyEoyxGf238pPPMsKNjPS3cC3sFzSW3cbnQA']['smappee']['main'],
-        electricityUsage: parsed['QmdhnZLVkkPyEoyxGf238pPPMsKNjPS3cC3sFzSW3cbnQA']['smappee']['usage']['electricity'],
-        electricityAppliances: parsed['QmdhnZLVkkPyEoyxGf238pPPMsKNjPS3cC3sFzSW3cbnQA']['smappee']['usage']['appliances'],
-        electricityEvents: parsed['QmdhnZLVkkPyEoyxGf238pPPMsKNjPS3cC3sFzSW3cbnQA']['smappee']['events'],
-        water: parsed['QmdhnZLVkkPyEoyxGf238pPPMsKNjPS3cC3sFzSW3cbnQA']['water']
+        windSpeed: parsed['QmULmQvxP7RYMHjQDcze6G5FoV4EaFKN5gC7Di7TrmUqKY']['data']['channel']['wind']['speed'],
+        atmosphere: parsed['QmULmQvxP7RYMHjQDcze6G5FoV4EaFKN5gC7Di7TrmUqKY']['data']['channel']['atmosphere'],
+        astronomy: parsed['QmULmQvxP7RYMHjQDcze6G5FoV4EaFKN5gC7Di7TrmUqKY']['data']['channel']['astronomy'],
+        conditions: parsed['QmULmQvxP7RYMHjQDcze6G5FoV4EaFKN5gC7Di7TrmUqKY']['data']['channel']['item']['condition'],
+        insideTemp: parsed['QmREQVyyNum1RVRW9b4kKYHGsmmRovTsWTaTuBej9JBWx6']['data']['parkertemperature'],
+        insidePressure: parsed['QmREQVyyNum1RVRW9b4kKYHGsmmRovTsWTaTuBej9JBWx6']['data']['parkerpressure'],
+        goalTemp: parsed['QmREQVyyNum1RVRW9b4kKYHGsmmRovTsWTaTuBej9JBWx6']['data']['parkergoaltemp'],
+        insideAltitude: parsed['QmREQVyyNum1RVRW9b4kKYHGsmmRovTsWTaTuBej9JBWx6']['data']['parkeraltitude'],
+        hvacStatus: parsed['QmTzKsdeNiTmpeHBhq9uA8QYYPBvRTJjdPU6usrbP3SFso']['data']['parkerheaterfan'],
+        hvacForce: parsed['QmTzKsdeNiTmpeHBhq9uA8QYYPBvRTJjdPU6usrbP3SFso']['data']['fan'],
+        if(){
+
+        }
       }
 
       _this.setState({state})
@@ -175,9 +173,7 @@ class HomePage extends Component {
     })
   }
 
-  render() {
-    const sound = (randomIntBtwNumbers(0,1))
-    const recentInteraction = (randomIntBtwNumbers(0,1))
+  render() {  
     const humidity = (this.state.state.atmosphere.humidity)
     const pressure = (this.state.state.atmosphere.pressure)
     const visibility = (this.state.state.atmosphere.visibility)
@@ -185,37 +181,23 @@ class HomePage extends Component {
     const sunset = (this.state.state.astronomy.sunset)
     const weatherTemp = (this.state.state.conditions.temp)
     const weatherText = (this.state.state.conditions.text)
-    const GPSlat = (this.state.state.elderGPS.lat)
-    const GPSlong = (this.state.state.elderGPS.long)
-    const xAccel = (this.state.state.elderAccelerometer.xAccel)
-    const yAccel = (this.state.state.elderAccelerometer.yAccel)
-    const zAccel = (this.state.state.elderAccelerometer.zAccel)
-    const O3 = (this.state.state.gasSensor.O3)
-    const CO = (this.state.state.gasSensor.CO)
-    const CO2 = (this.state.state.gasSensor.CO2)
-    const NO = (this.state.state.gasSensor.NO)
-    const NO2 = (this.state.state.gasSensor.NO2)
-    const CH4 = (this.state.state.gasSensor.CH4)
-    const H2 = (this.state.state.gasSensor.H2)
-    const elecMain = (this.state.state.energyMain['totalEnergy'])
-    const elecUsage = (this.state.state.electricityUsage['wattHours'])
-    const elecAppliances = (this.state.state.electricityAppliances)
-    const elecEvents = (this.state.state.electricityEvents)
-    const waterMain = (this.state.state.water['main'])
-    const waterAppliances = (this.state.state.water['appliances'])
-    const waterEvents = (this.state.state.water['events'])
+    const insideTemp = (this.state.state.insideTemp.data)
+    const insidePressure = (this.state.state.insidePressure.data)
+    const goalTemp = (this.state.state.goalTemp.data)
+    const insideAltitude = (this.state.state.insideAltitude.data)
+    const hvacStatus = (this.state.state.hvacStatus.data)
+    const hvacForce = (this.state.state.hvacForce.data)
+
     // const price0 = (this.state.state.price)? `$${this.state.state.price}` : ''
     // const price1 = (this.state.state[1].price)? `$${this.state.state[1].price}` : ''
     // const chargeTime0 = (this.state.state.timeCharging)? `Been charging for ${this.state.state.timeCharging.substring(0,6)} minutes` : ''
     // const chargeTime1 = (this.state.state[1].timeCharging)? `Been charging for ${this.state.state[1].timeCharging.substring(0,6)} minutes` : ''
     // const doing = (this.state.state.changeinPercent.includes('-')) ? 'poorly' : 'well'
     // const stockchange = (this.state.state.changeinPercent.includes('+')) ? 'increased' : 'decreased'
-    // <CarSpot sound={sound} recentInteraction={recentInteraction} windSpeed={this.state.state.windSpeed} left="100" top="100" atmosphere={this.state.state.atmosphere} astronomy={this.state.state.astronomy} conditions={this.state.state.conditions} elderHeartRate={this.state.state.elderHeartRate} elderLightSensor={this.state.state.elderLightSensor} elderGPS={this.state.state.elderGPS} elderAccelerometer={this.state.state.elderAccelerometer} gasSensor={this.state.state.gasSensor} energySmappee={this.state.state.energySmappee} water={this.state.state.water}/>
 
     return (
       <div>
-
-        <CarSpot sound={sound} humidity={humidity} pressure={pressure} visibility={visibility} sunrise={sunrise} sunset={sunset} weatherTemp={weatherTemp} weatherText={weatherText} GPSlat={GPSlat} GPSlong={GPSlong} xAccel={xAccel} yAccel={yAccel} zAccel={zAccel} O3={O3} CO={CO} CO2={CO2} NO={NO} NO2={NO2} CH4={CH4} H2={H2} elecMain={elecMain} elecUsage={elecUsage} elecAppliances={elecAppliances} elecEvents={elecEvents} waterMain={waterMain} waterAppliances={waterAppliances} waterEvents={waterEvents} recentInteraction={recentInteraction} windSpeed={this.state.state.windSpeed} left="100" top="100" elderHeartRate={this.state.state.elderHeartRate} elderLightSensor={this.state.state.elderLightSensor} elecAppliances={elecAppliances}/>
+        <CarSpot humidity={humidity} pressure={pressure} visibility={visibility} sunrise={sunrise} sunset={sunset} weatherTemp={weatherTemp} weatherText={weatherText} insideTemp={insideTemp} insidePressure={insidePressure} goalTemp={goalTemp} insideAltitude={insideAltitude} hvacStatus={hvacStatus} hvacForce={hvacForce} windSpeed={this.state.state.windSpeed} left="100" top="100"/>
       </div>
     )
   }
