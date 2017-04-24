a168fdeb84d9a11ed197618f69f23b4b8cb0a08c
Gavin McDermott <gavinmcdermott@gmail.com>
2017-04-24 16:58:23 -0700

    add new id for room temp sensor; add data package notes

--- sensor.js
+++ sensor.js
@@ -1,3 +1,23 @@
+// Sensor Notes:
+
+// Particle event package:
+// {
+//   data: '75',
+//   ttl: '60',
+//   published_at: '2017-04-24T23:54:08.732Z',
+//   coreid: '200033000d51353432383931',
+//   name: 'RoomTemperature '
+// }
+
+// Nomad data package published:
+// {
+//   roomtemperature: {
+//     data: '75',
+//     time: '2017-04-24T23:54:08.732Z',
+//     description: 'IDEO CoLab room temperature'
+//   }
+// }
+
 const Nomad = require('nomad-stream')
 const moment = require('moment')
 const Particle = require('particle-api-js')
@@ -9,35 +29,20 @@ const nomad = new Nomad()
 
 // Particle Device Setup
 // Atomic node 1
-const deviceID = '2b0045000247353138383138'
+const deviceID = '200033000d51353432383931'
 
 let instance = null
 let lastPub = null
 let token
 
-const defaultPublishData = { 
-  parkertemperature: {
-    data: "",
-    time: "",
-    description: "The Status of the temperature in the room at IDEO CoLab"
-  },
-  parkerpressure: {
-    data: "",
-    time: "",
-    description: "The Status of the pressure in the room at IDEO CoLab"
-  },
-  parkergoaltemp: {
-    data: "",
-    time: "",
-    description: "The Status of the goal temperature in the room at IDEO CoLab"
-  },
-  parkeraltitude: {
-    data: "",
-    time: "",
-    description: "The Status of the altitude in the room at IDEO CoLab"
+const defaultPublishData = {
+  roomTemperature: {
+    data: '',
+    time: '',
+    description: 'IDEO CoLab room temperature'
   }
 }
-const timeBetween =  30 * 60 * 1000 // 2 minutes
+const timeBetween =  60 * 1000 * .5 // 2 minutes
 const timeThreshold = 4 * 60 * 60 * 1000 // 4 hours
 
 class DataMaintainer {
@@ -87,22 +92,24 @@ particle.login(credentials)
   })
   .then((n) => {
     instance = n
+    console.log('about to publish root')
     return instance.publishRoot('hello this data from the home of colab cambridge')
   })
   .then(() => {
     //declaring last publish date
     lastPub = getTime()
+    console.log('root published')
     return particle.getEventStream({ deviceId: deviceID, auth: token })
   })
   .then(s => {
     stream = s
     stream.on('event', data => {
-      console.log(data)
+      console.log('particle event data arrived:', data)
       try{dataManager.setValue(data.name, {data: data.data, time: data.published_at})}
       catch(err){
         console.log("DataMaintainer failed with error of " + err)
       }
-      // this determines frequency of transmission 
+      // this determines frequency of transmission
       let currentTime = getTime()
       let timeSince = currentTime - lastPub
       if (timeSince >= timeBetween){
@@ -117,7 +124,7 @@ particle.login(credentials)
 
           instance.publish(dataManager.toString())
             .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
-          dataManager.clear()  
+          dataManager.clear()
           lastPub = currentTime
         }
       }
@@ -129,7 +136,7 @@ particle.login(credentials)
         console.log("***************************************************************************************")
         console.log(dataManager.getAll())
         console.log("***************************************************************************************")
-        dataManager.clear()  
+        dataManager.clear()
         lastPub = currentTime
       }
     })
